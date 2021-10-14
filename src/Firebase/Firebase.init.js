b03c060e143229c3e2420a771aace26e3529b93b
Ratry6 <tanrat0602@gmail.com>
2021-10-14 21:47:12 +0600

    privateRoute added

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -8,32 +8,3 @@ const InitializeAuthentication = () =>{
 export default InitializeAuthentication;
 
 
-// /steps for authentication
-
-// Step:1- Initial Setup
-//  1. firebase.com : create project
-// 2. project settings : create web app
-// 3. get configuration ( firebase.config.js in src-> firebase folder)
-// 4.initialize firebase ( firebase.init.js in src-> firebase folder)
-// 5. sign-in-method enabled
-
-//------------
-
-//Step:2-
-//1.Create Login Component
-//2.Create Register Component
-//3.Create route for login & register
-//--------------
-//step:3-
-//1. set up signInWithGoogle
-//2. set up Sign Out Method
-//3.use State 
-//4.special observer (onAuthChanged)
-//5.return necessary methods and states from useFirebase
-//-------------
-//step-4:
-//1.create an auth context
-//2.create context provider
-//3.set context value in AuthProvider
-//4. use auth provider
-//5.create useAuth Hook
